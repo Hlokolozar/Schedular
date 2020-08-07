58470a5ce75cf2684d0e72bb5aa44c26fda777ce
Lehlohonolo Motsoeneng <joseleh@live.com>
2020-08-07 11:11:31 +0200

    CRUD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { DragDropModule } from "@angular/cdk/drag-drop";
 import { NgModule } from "@angular/core";
-
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+
 import { TodoComponent } from "./todo/todo.component";
 
 @NgModule({
   declarations: [AppComponent, TodoComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    DragDropModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Inject } from "@angular/core";
 import { ServicesService } from "../services/services.service";
-import { identifierModuleUrl } from "@angular/compiler";
+import { CdkDragDrop, moveItemInArray } from "@angular/cdk/drag-drop";
+import { Todo } from "../interfaces/Todo";
+import { DOCUMENT } from "@angular/common";
 
 @Component({
   selector: "app-todo",
@@ -8,10 +10,28 @@ import { identifierModuleUrl } from "@angular/compiler";
   styleUrls: ["./todo.component.css"],
 })
 export class TodoComponent implements OnInit {
-  constructor(private api: ServicesService) {}
-  todo = [];
-  newItem;
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private api: ServicesService
+  ) {}
 
+  todo: Todo[];
+  newItem;
+  movies = [
+    "Episode I - The Phantom Menace",
+    "Episode II - Attack of the Clones",
+    "Episode III - Revenge of the Sith",
+    "Episode IV - A New Hope",
+    "Episode V - The Empire Strikes Back",
+    "Episode VI - Return of the Jedi",
+    "Episode VII - The Force Awakens",
+    "Episode VIII - The Last Jedi",
+    "Episode IX – The Rise of Skywalker",
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+  }
   public addTodo(title, description, dueDate) {
     var id = this.todo.length + 1;
     const newItem = {
@@ -19,13 +39,11 @@ export class TodoComponent implements OnInit {
       title: title,
       description: description,
       status: "New",
+      editing: false,
       dueDate: dueDate,
     };
     this.todo.push(newItem);
     this.addItem(newItem);
-  }
-  ngOnInit() {
-    this.getTodoItems();
   }
 
   public getTodoItems = () => {
@@ -50,8 +68,17 @@ export class TodoComponent implements OnInit {
       }
     );
   };
-
+  editItem(item): void {
+    item.editing = true;
+  }
+  doneEditing(item) {
+    item.editing = false;
+  }
   public deleteItem(item) {
     this.todo = this.todo.filter((t) => t.title !== item.title);
+  }
+
+  ngOnInit() {
+    this.getTodoItems();
   }
 }
