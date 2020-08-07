c06cdb40d4ecdd1e2c0472ca445bde41cd05f62e
Lehlohonolo Motsoeneng <joseleh@live.com>
2020-08-07 11:36:34 +0200

    Drag and Drop

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Inject } from "@angular/core";
 import { ServicesService } from "../services/services.service";
 import { CdkDragDrop, moveItemInArray } from "@angular/cdk/drag-drop";
 import { Todo } from "../interfaces/Todo";
-import { DOCUMENT } from "@angular/common";
 
 @Component({
   selector: "app-todo",
@@ -10,27 +9,13 @@ import { DOCUMENT } from "@angular/common";
   styleUrls: ["./todo.component.css"],
 })
 export class TodoComponent implements OnInit {
-  constructor(
-    @Inject(DOCUMENT) private document: Document,
-    private api: ServicesService
-  ) {}
+  constructor(private api: ServicesService) {}
 
-  todo: Todo[];
-  newItem;
-  movies = [
-    "Episode I - The Phantom Menace",
-    "Episode II - Attack of the Clones",
-    "Episode III - Revenge of the Sith",
-    "Episode IV - A New Hope",
-    "Episode V - The Empire Strikes Back",
-    "Episode VI - Return of the Jedi",
-    "Episode VII - The Force Awakens",
-    "Episode VIII - The Last Jedi",
-    "Episode IX – The Rise of Skywalker",
-  ];
+  public todo: Todo[];
+  public newItem: Todo;
 
   drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+    moveItemInArray(this.todo, event.previousIndex, event.currentIndex);
   }
   public addTodo(title, description, dueDate) {
     var id = this.todo.length + 1;
